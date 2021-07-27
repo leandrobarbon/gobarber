8903ab26249de95201ef1194cb53352d83daf749
Leandro da silva <leandrosilva.hair@gmail.com>
2021-07-27 09:03:20 -0300

    Update initial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Globalstyle from './styles/global';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+
+function App() {
+  return (
+    <>
+      <SignIn />
+      <Globalstyle />
+    </>
+  );
+}
+
+export default App;
